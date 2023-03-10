0a72b60f8469ef5c57ced5bcde9519436d648f44
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-03-10 14:16:51 -0800

    script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num_args))
     for i in range(num_args):
-        print("{}:{}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
