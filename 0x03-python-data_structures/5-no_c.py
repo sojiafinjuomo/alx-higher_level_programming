6bf7fa8c8fbe3fc063c26955129a551eddf1911a
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-03-13 14:14:39 -0700

    char in string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    if my_string:
-        new_string = []
-        for i in my_string:
-            if i not in 'Cc':
-                new_string.append(i)
-        my_string = ''.join(new_string)
-        return my_string
+    my_string = ''.join(char for char in my_string if char not in 'Cc')
+    return my_string
