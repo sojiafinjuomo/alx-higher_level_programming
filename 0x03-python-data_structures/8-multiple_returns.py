e8a1a0a10d3da52f0f1f0c4674046f9346d236cb
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-03-14 02:46:57 -0700

    tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return None
+    else:
+        Length = (len(sentence))
+        first = sentence[0]
+        return("Length : {:d} - First character: {}".format(Length, first))
