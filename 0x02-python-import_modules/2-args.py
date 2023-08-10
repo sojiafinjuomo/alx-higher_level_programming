9ddabd8327812cf55563498eab72e7ebe4548fa7
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-08-10 18:47:33 +0000

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_argvs = len(sys.argv)-1
+    if num_argvs == 0:
+        print("{:d} arguments.".format(num_argvs))
+    elif num_argvs == 1:
+        print("{:d} argument: ".format(num_argvs))
+    else:
+        print("{:d} arguments: ".format(num_argvs))
+
+    if num_argvs >= 1:
+        num_argvs = 0
+        for arg in sys.argv:
+            if num_argvs != 0:
+                print("{}: {}".format(num_argvs, arg))
+            num_argvs += 1
