228faa56413a92a5579ba01c0816376f12b7c5bc
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-08-14 15:05:00 +0000

    2-replace_in_list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= length:
         return my_list
     else:
-        my_list[index] = element
+        my_list[idx] = element
         return my_list
