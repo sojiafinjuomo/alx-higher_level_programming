230d135d64e10a538cc65132643ebe307040646c
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-08-14 18:24:45 +0000

    4-new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     length = len(my_list)
-    if idx < 0 or idx > length:
+    if idx < 0 or idx >= length:
         return my_list.copy()
     if my_list:
         new_list = my_list.copy()
