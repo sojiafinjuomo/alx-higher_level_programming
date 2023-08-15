25cc02af2aa1c435fb5da351a77d7034b079a26a
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-08-15 21:48:08 +0000

    11-delete_at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    for i in my_list:
+        if my_list.index(i) != idx:
+            new_list.append(i)
+    return new_list
