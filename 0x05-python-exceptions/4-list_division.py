1e7a928b81c8a483b2fa8260a52485a98e5ecffb
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-08-28 22:29:29 +0000

    4-list_division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,10 +8,13 @@ def list_division(my_list_1, my_list_2, list_length):
             quotient = 0
         except TypeError:
             print("wrong type")
+            quotient = 0
         except ZeroDivisionError:
             print("division by 0")
+            quotient = 0
         except IndexError:
             print("out of range")
+            quotient = 0
         finally:
             new_list.append(quotient)
     return (new_list)
