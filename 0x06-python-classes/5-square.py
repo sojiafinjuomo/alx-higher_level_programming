5ec212d94e2ca45097db9c70f92f1c51751278e9
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-09-01 03:14:10 -0700

    5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,7 +25,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """defines area of a square"""
