f032684c8e89e3f80bf137bc6c90a84df60bb165
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-09-01 01:57:51 -0700

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size(int): size of sides of square
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -21,12 +21,12 @@ class Square:
     @size.setter
     def size(self, value):
         """method checks the state of size"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """defines area of a square"""
