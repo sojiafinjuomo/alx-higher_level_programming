797e80f598f5bfc40cfcda89a3a081385ca02b97
sojiafinjuomosoji <afinjuomosoji@gmail.com>
2023-10-19 20:21:15 +0000

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -10,4 +10,4 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 		id INT NOT NULL PRIMARY KEY,
 		state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-		name VARCHAR(256));
+		name VARCHAR(256) NOT NULL);
