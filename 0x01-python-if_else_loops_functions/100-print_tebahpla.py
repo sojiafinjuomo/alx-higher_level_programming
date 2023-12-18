62791569a8b043ff5a0d34177804c9e0c0d03f14
sojiafinjuomo <117821609+sojiafinjuomo@users.noreply.github.com>
2023-12-18 12:35:54 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(i, chr(i - 33)), end="")
--- 0x01-python-if_else_loops_functions/11-pow.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def pow(a, b):
-    result = a ** b
-    return result
