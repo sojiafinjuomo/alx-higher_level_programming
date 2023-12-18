11d143bed9d1412598c6f8fc7ad3ecd2ed0e9e62
sojiafinjuomo <117821609+sojiafinjuomo@users.noreply.github.com>
2023-12-18 12:39:56 +0100

    Delete 0x05-python-exceptions directory

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        quotient = a / b
-    except ZeroDivisionError:
-        quotient = None
-    finally:
-        print("Inside result: {}".format(quotient))
-    return (quotient)
